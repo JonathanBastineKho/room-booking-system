d184d2d05cdeb62f370d6f866f2bce37f222ccb2
Jonathan Bastine Kho <jonathanbastine@gmail.com>
2023-04-14 13:54:28 +0800

    Initial Boilerplate code

---
+++ server/config.py
@@ -0,0 +1,9 @@
+import os 
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-flask-key'
+    FLASK_APP = os.environ.get('FLASK_APP') or 'application.py'
