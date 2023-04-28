3aa71c84254438b0b2a028b70e63a9aee2a1d333
edbertekaputera <edbertekaputera@gmail.com>
2023-04-28 15:45:06 +0800

    Temp fix for merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import logo from './logo.svg';
 
-export default function App() {
+function App() {
   return (
-	<div>App</div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+		  I LIKE TO EAT BLOCK A FOOD BECAUSE BLOCK B FOOD IS MID.
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
