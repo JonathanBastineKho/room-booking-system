3e67a39d976b68f6e6de81a68517c3ce5e63d321
Jonathan Bastine Kho <jonathanbastine@gmail.com>
2023-05-03 21:04:07 +0800

    Update LoginPage.js

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,28 +1,10 @@
 import React from "react";
-import SearchCard from "../Components/Search/SearchCard";
-import UserCurrentTable from "../Components/Table/User/UserCurrentTable";
-import FilterSortAccordion from "../Components/Search/FilterSortAccordion";
-import StaffPromoTable from "../Components/Table/Staff/StaffPromoTable";
-import AdminRoomTable from "../Components/Table/Admin/AdminRoomTable";
-import UserPastTable from "../Components/Table/User/UserPastTable";
-import StaffRoomTable from "../Components/Table/Staff/StaffRoomTable";
-import ScheduleTable from "../Components/Table/User/ScheduleTable";
-import AdminTransactionTable from "../Components/Table/Admin/AdminTransactionTable";
+import LoginCard from "../Components/Login/LoginCard";
 
 function LoginPage() {
 	return (
-		<div className="flex flex-col justify-center w-full items-center align-middle gap-5">
-			LoginPage
-			<SearchCard />
-			<UserCurrentTable />
-			<UserPastTable />
-			< ScheduleTable />
-			<FilterSortAccordion />
-			<StaffRoomTable />
-			<StaffPromoTable />
-			<AdminRoomTable />
-			<AdminTransactionTable />
-			<div className="h-96"></div>
+		<div className="flex justify-center items-center h-screen">
+			<LoginCard />
 		</div>
 	);
 }
